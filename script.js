96a3807f2c5361794927da1d96cf8133dfa487d5
Evgeny <220798h@gmail.com>
2020-07-18 18:01:10 +0300

    1 commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+    menu.onclick = function myFunction() {
+        var x = document.getElementById("MyTopnav");
+     
+        if(x.className === "topnav") {
+                 x.className += " responsive";
+        } else{
+            x.className = "topnav";
+        }
+     }
+})
+
+
